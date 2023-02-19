c3e04b9523f8489d9334bdd47362f6c8e065ca4e
Clayton Becker <claytonbecker@Claytons-MacBook-Air.local>
2023-02-19 11:58:35 -0800

    Part 4 Commit

--- ps6_becker_clayton.R
+++ ps6_becker_clayton.R
@@ -155,6 +155,50 @@ for (i in 1:length(files)) {
   dfs[[i]] <- df 
 }
 
+
+## -----------------------------------------------------------------------------
+## Part 4 - Label each question using comments
+## -----------------------------------------------------------------------------
+
+# Question 1 
+
+hd2019 <- dfs[[3]]
+
+# Question 2 
+    
+hd2019_subset <- hd2019 %>% mutate(
+  HBCU = if_else(HBCU == 1, 1, 0),
+  TRIBAL = if_else(TRIBAL == 1, 1, 0),
+  HOSPITAL = if_else(HOSPITAL == 1, 1, 0)) %>% select(HBCU, TRIBAL, HOSPITAL)
+
+
+# Question 3 
+
+for (i in names(hd2019_subset)) {
+  writeLines(str_c("Column Name = " , i))
+}
+
+# Question 4
+
+for (i in names(hd2019_subset)) {
+  writeLines(str_c("Number of " , i, " = ", sum(hd2019_subset[[i]])))
+}
+
+# Question 5
+
+nums <- vector()
+
+for (i in 1:length(hd2019_subset)) {
+  nums[[i]] <- str_c(sum(hd2019_subset[[i]]))
+}
+  
+
+ggplot(data.frame(nums), aes(seq_along(nums), nums)) +
+  geom_bar(stat = 'identity') +
+  scale_x_continuous(breaks = seq_along(hd2019_subset), labels = names(hd2019_subset)) +
+  xlab(NULL) + ylab(NULL)
+
+
 ## -----------------------------------------------------------------------------
 ## END SCRIPT
 ## -----------------------------------------------------------------------------
